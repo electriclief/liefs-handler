9c152889f88d4e1599858b2fa154b5be12e33db3
Leafdriving <leafdriving@hotmail.com>
2017-02-04 17:56:31 -0500

    mdown09

--- index_.ts
+++ index_.ts
@@ -412,7 +412,7 @@ declare var jasmineTests: boolean;
       let dragDiff: number = (Dragbar.direction ? e.clientX : e.clientY) - Dragbar.dragstart;
       let newCurrent: number = vpx(pItem.current) + dragDiff;
 
-      if (pItem.min && (newCurrent > vpx(pItem.min))) newCurrent = vpx(pItem.min);
+      if (pItem.min && (newCurrent < vpx(pItem.min))) newCurrent = vpx(pItem.min);
       if (pItem.max && (newCurrent > vpx(pItem.max))) newCurrent = vpx(pItem.max);
 
       console.log(newCurrent);
